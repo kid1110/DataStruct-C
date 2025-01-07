dc0ecd49a8dd4c1cfa644e7ef18abe2590dcc3f5
kid <1050109853@qq.com>
2025-01-07 18:29:46 +0800

    feat: add select sort

--- sorts/sorts.c
+++ sorts/sorts.c
@@ -1,45 +1,58 @@
 #include "sorts.h"
 
-void swap(void* a, void* b,size_t size){ 
-    void* temp = malloc(size);
+void swap(void *a, void *b, size_t size)
+{
+    void *temp = malloc(size);
     memcpy(temp, a, size);
     memcpy(a, b, size);
     memcpy(b, temp, size);
     free(temp);
 }
 
-
-void bubble_sort(void* arr, size_t n, size_t size, Comparator cmp){
+void bubble_sort(void *arr, size_t n, size_t size, Comparator cmp)
+{
     int flag = 0;
-    for(int i = n-1;i>=0;i--){
-        for(int j  = 0; j <i;j++){
-            void* first = (char*)arr + j*size;
-            void* second = (char*)arr + (j+1)*size;
-            if(cmp(first,second)>0){
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            void *first = (char *)arr + j * size;
+            void *second = (char *)arr + (j + 1) * size;
+            if (cmp(first, second) > 0)
+            {
                 flag = 1;
-                swap(first,second, size);
+                swap(first, second, size);
             }
         }
-        if(flag == 0){
+        if (flag == 0)
+        {
             break;
         }
     }
 }
 
-void insert_sort(void* arr, size_t n, size_t size, Comparator cmp){
-    //TODO: add things here
+void insert_sort(void *arr, size_t n, size_t size, Comparator cmp)
+{
+    // TODO: add things here
 }
 
-void select_sort(void* arr, size_t n, size_t size, Comparator cmp){
-    //TODO: add things here
+void select_sort(void *arr, size_t n, size_t size, Comparator cmp)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            void *first = (char *)arr + i * size;
+            void *second = (char *)arr + j * size;
+            if (cmp(first, second) > 0)
+            {
+                swap(first, second, size);
+            }
+        }
+    }
 }
 
-
-void quick_sort(void* arr, size_t n, size_t size, Comparator cmp){
-    //TODO: add content 
+void quick_sort(void *arr, size_t n, size_t size, Comparator cmp)
+{
+    // TODO: add content
 }
-
-
-
-
-
